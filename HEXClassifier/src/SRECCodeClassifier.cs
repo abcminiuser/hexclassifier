82d9dcc2f139304b169ee0a1f553b2f23ebe84ec
Morten Engelhardt Olsen <moro.engelhardt@gmail.com>
2013-12-09 17:00:23 +0100

    Add MEF components for registration of the SREC parser

---
+++ HEXClassifier/src/SRECCodeClassifier.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+
+namespace FourWalledCubicle.HEXClassifier
+{
+    internal sealed class SRECCodeClassifier : IClassifier
+    {
+        private static readonly Dictionary<SRECParser.SRECEntryTypes, string> mClassifierTypeNames = new Dictionary<SRECParser.SRECEntryTypes, string>() {
+            { SRECParser.SRECEntryTypes.START_CODE, "srec.startcode" },
+            { SRECParser.SRECEntryTypes.BYTE_COUNT, "srec.bytecount" },
+            { SRECParser.SRECEntryTypes.ADDRESS, "srec.address" },
+            { SRECParser.SRECEntryTypes.RECORD_TYPE, "srec.recordtype" },
+            { SRECParser.SRECEntryTypes.DATA, "srec.data" },
+            { SRECParser.SRECEntryTypes.CHECKSUM, "srec.checksum" },
+        };
+
+        private readonly ITextBuffer mTextBuffer;
+        private readonly IClassificationTypeRegistryService mClassificationTypeRegistry;
+        private readonly List<ClassificationSpan> classifications = new List<ClassificationSpan>();
+
+#pragma warning disable 0067
+        public event EventHandler<ClassificationChangedEventArgs> ClassificationChanged;
+#pragma warning restore 0067
+
+        public SRECCodeClassifier(ITextBuffer buffer, IClassificationTypeRegistryService classifierTypeRegistry)
+        {
+            mTextBuffer = buffer;
+            mClassificationTypeRegistry = classifierTypeRegistry;
+        }
+
+        public IList<ClassificationSpan> GetClassificationSpans(SnapshotSpan span)
+        {
+            classifications.Clear();
+
+            if (span.Length == 0)
+                return classifications;
+
+            ITextSnapshotLine line = span.Start.GetContainingLine();
+
+            foreach (Tuple<SRECParser.SRECEntryTypes, SnapshotSpan> segment in SRECParser.Parse(line))
+            {
+                IClassificationType classificationType = mClassificationTypeRegistry.GetClassificationType(mClassifierTypeNames[segment.Item1]);
+                classifications.Add(new ClassificationSpan(segment.Item2, classificationType));
+            }
+
+            return classifications;
+        }
+    }
+}
---
+++ HEXClassifier/src/SRECContentDefinition.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Utilities;
+
+namespace FourWalledCubicle.HEXClassifier
+{
+    internal static class HEXContentDefinition
+    {
+        [Export]
+        [Name("srec")]
+        [BaseDefinition("code")]
+        internal static ContentTypeDefinition srecContentTypeDefinition { get; set; }
+
+        [Export]
+        [ContentType("srec")]
+        [FileExtension(".srec")]
+        internal static FileExtensionToContentTypeDefinition srecContentTypeDefinitionFileExtension { get; set; }
+
+    }
+}
