8fe79ba6243c24c709067af47a49b8c8e0368341
Morten Engelhardt Olsen <moro.engelhardt@gmail.com>
2013-12-20 22:09:21 +0100

    Start with new local dev project settings. NB: Needs to be reverted before upstream

---
+++ HEXClassifier/src/HexViewport.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.VisualStudio.Text.Classification;
+using System.Windows;
+
+namespace FourWalledCubicle.HEXClassifier.src
+{
+    [Name(HexViewport.MarginName)]
+    class HexViewport :IWpfTextViewMargin
+    {
+        public const string MarginName = "Hex Information";
+
+        private readonly IWpfTextView _textView;
+        private readonly IClassificationFormatMap _classificationFormatMap;
+        private readonly IEditorFormatMap _editorFormatMap;
+        private readonly FrameworkElement _element;
+
+        private bool _isDisposed = false;
+
+
+        public HexViewport(IWpfTextView textView, InformationMarginFactory factory)
+        {
+            _textView = textView;
+            _classificationFormatMap = factory.ClassificationMapService.GetClassificationFormatMap(textView);
+            _editorFormatMap = factory.EditorFormatSerivce.GetEditorFormatMap(textView);
+
+            _editorFormatMap.FormatMappingChanged += HandleFormatMappingChanged;
+            _textView.Closed += (sender, e) => _editorFormatMap.FormatMappingChanged -= HandleFormatMappingChanged;
+            textView.Options.OptionChanged += HandleOptionsChanged;
+        }
+
+        private void ThrowIfDisposed()
+        {
+            if (_isDisposed)
+                throw new ObjectDisposedException(MarginName);
+        }
+
+        private void HandleFormatMappingChanged(object sender, FormatItemsEventArgs e)
+        {
+            if (_isDisposed)
+                return;
+
+        }
+
+        private void HandleOptionsChanged(object sender, EditorOptionChangedEventArgs e)
+        {
+            if (!_isDisposed)
+                return;
+        }
+
+        #region IWpfTextViewMargin Members
+
+        public FrameworkElement VisualElement
+        {
+            get
+            {
+                ThrowIfDisposed();
+                return _element;
+            }
+        }
+
+        #endregion
+
+        #region ITextViewMargin Members
+
+        public double MarginSize
+        {
+            get
+            {
+                ThrowIfDisposed();
+                return this.ActualHeight;
+            }
+        }
+
+        public bool Enabled
+        {
+            get
+            {
+                ThrowIfDisposed();
+                return true;
+            }
+        }
+
+        public ITextViewMargin GetTextViewMargin(string marginName)
+        {
+            return (marginName == HexViewport.MarginName) ? (IWpfTextViewMargin)this : null;
+        }
+
+        public void Dispose()
+        {
+            if (!_isDisposed)
+            {
+                GC.SuppressFinalize(this);
+                _isDisposed = true;
+            }
+        }
+        #endregion
+    }
+}
---
+++ HEXClassifier/src/InformationMarginFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+
+namespace FourWalledCubicle.HEXClassifier.src
+{
+    [Export(typeof(IWpfTextViewMarginProvider))]
+    [Name(HexViewport.MarginName)]
+    [Order(After = PredefinedMarginNames.Spacer, Before = PredefinedMarginNames.Outlining)]
+    [MarginContainer(PredefinedMarginNames.LeftSelection)]
+    [ContentType("hex")]
+    [TextViewRole(PredefinedTextViewRoles.Editable)]
+    internal sealed class InformationMarginFactory : IWpfTextViewMarginProvider
+    {
+        public IWpfTextViewMargin CreateMargin(IWpfTextViewHost textViewHost, IWpfTextViewMargin containerMargin)
+        {
+            return new HexViewport(textViewHost.TextView, this);
+        }
+
+        [Import]
+        internal ITextDocumentFactoryService TextDocumentFactoryService { get; private set; }
+
+        [Import]
+        internal IClassificationFormatMapService ClassificationMapService { get; private set; }
+
+        [Import]
+        internal IEditorFormatMapService EditorFormatSerivce { get; private set; }
+    }
+}
