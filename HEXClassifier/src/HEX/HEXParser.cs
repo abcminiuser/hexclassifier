4fbabfbd57f2ad1d5be4342ae44d2001ff40c968
Dean Camera <dean@fourwalledcubicle.com>
2013-12-22 12:22:07 +1100

    Fix missing structure definition, move parser token enum into the main parser file.

--- HEXClassifier/src/HEX/HEXParser.cs
+++ HEXClassifier/src/HEX/HEXParser.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
-using Microsoft.VisualStudio.Text;
 using System.Globalization;
+using Microsoft.VisualStudio.Text;
 
 namespace FourWalledCubicle.HEXClassifier
 {
--- HEXClassifier/src/Parser.cs
+++ HEXClassifier/src/Parser.cs
@@ -1,9 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.Text;
 
 namespace FourWalledCubicle.HEXClassifier
 {
+    enum TokenEntryTypes
+    {
+        START_CODE,
+        BYTE_COUNT,
+        ADDRESS,
+        RECORD_TYPE,
+        DATA,
+        CHECKSUM,
+        CHECKSUM_BAD,
+    };
+
+    struct SpanClassification
+    {
+        public TokenEntryTypes Entry;
+        public SnapshotSpan Span;
+    }
+
     interface Parser
     {
         IEnumerable<SpanClassification> Parse(ITextSnapshotLine line);
